d89e17ccb249cc6cbab1c6862bc0e81dbfde4d5d
Relly <105762069+RellFrmHell@users.noreply.github.com>
2026-01-06 22:36:34 -0500

    Add Java files and README

---
+++ DifferenceObjectsClasses.java
@@ -0,0 +1,12 @@
+public class DifferenceObjectsClasses {
+
+    public static void main(String[] args) {
+
+        Person p1 = new Person("Alex", 21);
+        Person p2 = new Person("Jordan", 30);
+
+        p1.displayInfo();
+        System.out.println();
+        p2.displayInfo();
+    }
+}
